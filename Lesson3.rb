ac03ebfd52a2379202b52385c5e10ba33ecc26de
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-23 22:28:22 +0300

    added lesson3 refactoring

--- Lesson3.rb
+++ Lesson3.rb
@@ -1,2 +1,2 @@
 array_types = [10, "text", 3.14, 2**62, [], :one, Bignum, nil, true]
-array_types.each { |obj| puts obj.to_s + " - " + obj.class.to_s}
+array_types.each { |obj| puts "#{obj} - #{obj.class}"}
