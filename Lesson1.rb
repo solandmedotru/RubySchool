c8fad4321bf581533663aef7732d250c37d4d799
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-23 16:45:50 +0300

    Change numbers to variables.

--- Lesson1.rb
+++ Lesson1.rb
@@ -1,2 +1,5 @@
-your_salary = 10000 # variable salary in dollar in month
-puts "Salary = $#{your_salary}"
+salary = 10000 # variable salary in Euro in month
+tax         = 13
+puts "Salary = #{ salary } Euro"
+puts "Tax = " << tax.to_s << "%" # add rax to string
+puts "Final = " + (salary - salary * tax).to_s + " Euro"
