03b1d45198e61d5b79e0c56a85e88f13238e4546
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-23 16:46:16 +0300

    Change format

--- Lesson1.rb
+++ Lesson1.rb
@@ -1,5 +1,5 @@
 salary = 10000 # variable salary in Euro in month
-tax         = 13
+tax    = 13
 puts "Salary = #{ salary } Euro"
 puts "Tax = " << tax.to_s << "%" # add rax to string
 puts "Final = " + (salary - salary * tax).to_s + " Euro"
