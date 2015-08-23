7e46fdfe0400aca2f2c92ab9d6b0b61f7c813e95
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-24 01:28:07 +0300

    refactoring

--- Lesson5.rb
+++ Lesson5.rb
@@ -1,7 +1,14 @@
 
-1.upto(100) do |n|
+1.upto(10) do |n|
   if n.odd?
     puts "Число #{n} - Нечетное"
     else puts "Число #{n} - Четное"
+  end
+end
+
+5.downto(-5) do |n|
+  if n.odd?
+    puts "Число #{n} - Нечетное"
+  else puts "Число #{n} - Четное"
   end
 end
