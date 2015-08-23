6f1fe15392a0df06355838f7f069f8f5174f6fef
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-23 19:44:54 +0300

    Change format output

--- Lesson1.rb
+++ Lesson1.rb
@@ -1,5 +1,16 @@
-salary = 10000 # variable salary in Euro in month
-tax    = 13
+salary        = 10000 # variable salary in Euro in month
+tax           = 0.13
+MONTH_IN_YEAR = 12
+
 puts "Salary = #{ salary } Euro"
-puts "Tax = " << tax.to_s << "%" # add rax to string
-puts "Final = " + (salary - salary * tax).to_s + " Euro"
+puts "Tax = " << (tax * 100).to_s << "%" # add rax to string
+
+month_salary = (salary - salary * tax)
+puts 'Final = ' + month_salary.to_s + " Euro"
+
+puts "Year salary without Tax = #{ month_salary * MONTH_IN_YEAR } Euro"
+key = ''
+while key != 'exit' do
+  puts "To finish type 'exit' and press Enter!"
+  key = gets.chomp
+end
