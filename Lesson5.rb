fb868bca1a02e2c9e1a67560510d599e9f7e18e2
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-24 12:50:35 +0300

    added homework from 6 lessson

--- Lesson5.rb
+++ Lesson5.rb
@@ -1,14 +1,25 @@
-
 1.upto(10) do |n|
   if n.odd?
     puts "Число #{n} - Нечетное"
-    else puts "Число #{n} - Четное"
+  else
+    puts "Число #{n} - Четное"
   end
 end
 
 5.downto(-5) do |n|
   if n.odd?
     puts "Число #{n} - Нечетное"
-  else puts "Число #{n} - Четное"
+  else
+    puts "Число #{n} - Четное"
   end
 end
+
+say = "Hello"
+10.times do |x|
+  print say
+  x.times do
+    print "!"
+  end
+  puts
+end
+
