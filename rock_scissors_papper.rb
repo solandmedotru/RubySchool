a9fcfc4dc17f433a0d32ed2f0ff4545f01b4c5bb
Andrey Solomin <solomin.andrey@gmail.com>
2015-08-28 12:05:26 +0300

    Refactoring code

--- rock_scissors_papper.rb
+++ rock_scissors_papper.rb
@@ -1,52 +1,36 @@
 arr = [:rock, :scissors, :paper]
 
 def get_answer(arr)
-  puts "Choose 1 - rock, 2 - scissors, 3 - paper, or 0 - exit"
+  puts 'Choose 1 - rock, 2 - scissors, 3 - paper, or 0 - exit'
   ch = gets.chomp.strip.to_i
   if ch == 0
-    puts "You choosed EXIT"
+    puts 'You choosed EXIT'
     exit
   else
     puts "You choosed #{arr[ch-1].to_s.upcase}!!!"
-    return ch
+    ch
   end
 end
 
 def result(arr)
-  answer = arr[get_answer(arr) - 1]
+  answer      = arr[get_answer(arr) - 1]
   comp_answer = arr[rand(0..2)]
   puts "I choosed #{comp_answer.to_s.upcase}"
 
-  if answer == :rock && comp_answer == :rock
-    return "="
-  end
-  if answer == :rock && comp_answer == :scissors
-    return "You WIN"
-  end
-  if answer == :rock && comp_answer == :paper
-    return "You LOOS"
-  end
+  'Nobody wins' if answer == comp_answer
 
+  'You WIN' if answer == :rock && comp_answer == :scissors
 
-  if answer == :scissors && comp_answer == :rock
-    return "You LOOS"
-  end
-  if answer == :scissors && comp_answer == :scissors
-    return "="
-  end
-  if answer == :scissors && comp_answer == :paper
-    return "You WIN"
-  end
+  'You LOOS' if answer == :rock && comp_answer == :paper
 
-  if answer == :paper && comp_answer == :rock
-    return "You WIN"
-  end
-  if answer == :paper && comp_answer == :scissors
-    return "You LOOS"
-  end
-  if answer == :paper && comp_answer == :paper
-    return "="
-  end
+  'You LOOS' if answer == :scissors && comp_answer == :rock
+
+  'You WIN' if answer == :scissors && comp_answer == :paper
+
+  'You WIN' if answer == :paper && comp_answer == :rock
+
+  'You LOOS' if answer == :paper && comp_answer == :scissors
+
 end
 
 while true do
